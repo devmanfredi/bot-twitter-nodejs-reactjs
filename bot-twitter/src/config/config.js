a879decedf545836129223107aee5d1a6866b0ea
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-07-17 01:20:02 -0300

    correção de configuração de acesso às apis do twitter

---
+++ bot-twitter/src/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  //API key
+  consumer_key:'pRxJGbcBxYWHIUezqJcobicd8',
+  //API secret key
+  consumer_secret:'UDk4wIEHQxBkzRrhLlC6vjmCCDNXeiqTXDewcRv4C1NS07PXhF',
+  //Access token
+  access_token:'1226236396467314688-QuxolpOJ3kMNNtWTGnKkYB3RtWcat4',
+  //Access token secret 
+  access_token_secret:'fgXH2aar3QPfUkdabp22hulTrzbcvnrxbHHgXcoBTR0uS'
+}
